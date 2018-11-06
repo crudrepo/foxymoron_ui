1218c3a0cb5779a72417fe2f1c0a8efd0cf79ae8
Tejas Vesvikar <tejas.vesvikar@zycus.com>
2018-11-06 14:11:20 +0530

    Changes

--- foxymoron_ui/ui/src/app/ass1/ass1.component.ts
+++ foxymoron_ui/ui/src/app/ass1/ass1.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ass1.component.css']
 })
 export class Ass1Component implements OnInit {
-
+  showResult:boolean = false;
+  msg:string ="";
+  str:string ="";
   constructor() { }
 
   ngOnInit() {
   }
+  onSubmitTemplateBased() {
+      let num = parseInt(this.str);
+      let flag = 1;
+      for(let i=2;i<num/2;i++)
+      {
+        if(num % i == 0)
+        {
+          flag = 0;
+          break;
+        }
+      }
+      this.showResult = true;
+      if(flag == 1)
+      {
+        this.msg = num+" is prime number";
+      }
+      else
+      {
+        this.msg = num+" is not prime number";
+      }
+    }
+  }
 
-}
--- foxymoron_ui/ui/src/app/ass3/ass3.component.ts
+++ foxymoron_ui/ui/src/app/ass3/ass3.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ass3.component.css']
 })
 export class Ass3Component implements OnInit {
-
+  showResult:boolean = false;
+  msg:string ="";
+  str:string ="";
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmitTemplateBased() {
+    let num = parseInt(this.str);
+    let str = "0 1";  
+    let a=0,b=1,temp;
+    for(let i=1;i<num-1;i++)
+    {
+      temp = a+b;
+      a = b;
+      b = temp;
+      str = str + " "+ temp;
+    }
+    this.showResult = true;
+    this.msg = str;
+  }
 }
